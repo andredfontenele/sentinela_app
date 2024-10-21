07d885efbc1d3390b07887c8280c970c747c1ab2
Andre <andrefonteneledev@gmail.com>
2024-10-21 10:08:06 -0300

    feat: CarroRepository e CarroController adicionados

--- src/main/java/com/sentinela/application/Controllers/CarroController.java
+++ src/main/java/com/sentinela/application/Controllers/CarroController.java
@@ -1,5 +1,28 @@
 package com.sentinela.application.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sentinela.application.Entity.Carro;
+import com.sentinela.application.Repository.CarroRepository;
+
+
+@RestController
+@RequestMapping("/carro")
 public class CarroController {
-    
+        
+        @Autowired
+        private CarroRepository carroRepository;
+
+        //surpresswarning?
+        @SuppressWarnings("rawtypes")
+        @PostMapping("/insertCarro")
+        public ResponseEntity insertCarro(@RequestBody Carro carro){
+            return null;
+            
+    }
 }

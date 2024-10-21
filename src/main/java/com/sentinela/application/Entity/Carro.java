773b2d3bb1da3e2cc7d7e40944efdfa4dcdc26fc
Andre <andrefonteneledev@gmail.com>
2024-10-21 08:58:36 -0300

    feat: Entidade Carro adicionada

--- src/main/java/com/sentinela/application/Entity/Carro.java
+++ src/main/java/com/sentinela/application/Entity/Carro.java
@@ -1,5 +1,82 @@
 package com.sentinela.application.Entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "carros")
 public class Carro {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer Id;
+
+    private String nome;
+    private float preco;
+    private int ano;
+    private String marca;
+    private String cidade;
+
+    @Column(name = "placa")
+    private String placa;
+
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setYear(int ano) {
+        this.ano = ano;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlace(String placa) {
+        this.placa = placa;
+    }
 }

9b8cc07e835013892e8edc18b5db5fde80ddd0d6
Andre <andrefonteneledev@gmail.com>
2024-10-22 08:55:10 -0300

    feat: Database Model criado

---
+++ db/Model.sql
@@ -0,0 +1,24 @@
+DROP TABLE usuarios;
+DROP TABLE carros;
+
+CREATE TABLE usuarios (
+	"id" bigserial NOT NULL,
+	"nome" varchar(255) NOT NULL,
+	"email" varchar(255) NOT NULL,
+	"senha" varchar(255) NOT NULL,
+	"qrcode" varchar(255) NULL,
+	CONSTRAINT usuarios_pkey PRIMARY KEY (id)
+);
+
+
+CREATE TABLE carros (
+	"id" bigserial NOT NULL,
+	"nome" varchar(255) NOT NULL,
+	"preco" float8 NOT NULL,
+	"ano" int4 NOT NULL,
+	"marca" varchar(255) NOT NULL,
+	"cidade" varchar(255) NOT NULL,
+	"placa" varchar(255) NOT NULL,
+	"qrcode" varchar(255) NULL,
+	CONSTRAINT carros_pkey PRIMARY KEY (id)
+);
